76a71965e2faa74bb0f4d9e9b5e75af7ef97170b
Han Hoa <hanhoa1997@gmail.com>
2023-07-19 22:31:03 +0700

    update1

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,126 @@
+import React, { useState } from "react";
+
+import { FCheckbox, FormProvider, FTextField } from "../components/form";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
+
+import useAuth from "../hooks/useAuth";
+
+import { useLocation, useNavigate, Link as RouterLink } from "react-router-dom";
+
+import {
+  Alert,
+  Container,
+  Stack,
+  Link,
+  InputAdornment,
+  IconButton,
+} from "@mui/material";
+import { Visibility } from "@mui/icons-material";
+import { VisibilityOff } from "@mui/icons-material";
+import { LoadingButton } from "@mui/lab";
+
+const LoginSchema = Yup.object().shape({
+  email: Yup.string().email("Invalid email").required("Email is required"),
+  password: Yup.string().required("Password is required"),
+});
+
+const defaultValues = {
+  email: "",
+  password: "",
+  remember: false,
+};
+
+function LoginPage() {
+  const auth = useAuth();
+  const methods = useForm({
+    resolver: yupResolver(LoginSchema),
+    defaultValues,
+  });
+
+  const {
+    handleSubmit,
+    reset,
+    setError,
+    formState: { errors, isSubmitting },
+  } = methods;
+
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [showPassword, setShowPassword] = useState(false);
+
+  const onSubmit = async (data) => {
+    const from = location.state?.from?.pathname || "/";
+    let { email, password } = data;
+
+    try {
+      await auth.login({ email, password }, () => {
+        navigate(from, { replace: true });
+      });
+    } catch (error) {
+      reset();
+      setError("responseError", error);
+    }
+  };
+  return (
+    <Container maxWidth="xs">
+      <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+        <Stack spacing={3}>
+          {!!errors.responseError && (
+            <Alert severity="error">{errors.responseError.message}</Alert>
+          )}
+          <Alert severity="info">
+            Don't have an account?{" "}
+            <Link variant="subtitle2" component={RouterLink} to="/register">
+              Get started
+            </Link>
+          </Alert>
+
+          <FTextField name="email" label="Email address" />
+          <FTextField
+            name="password"
+            label="Password"
+            type={showPassword ? "text" : "password"}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    onClick={() => setShowPassword(!showPassword)}
+                    edge="end"
+                  >
+                    {showPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Stack>
+
+        <Stack
+          direction="row"
+          alignItems="center"
+          justifyContent="space-between"
+          sx={{ my: 2 }}
+        >
+          <FCheckbox name="remember" label="Remember me" />
+          <Link component={RouterLink} variant="subtitle2" to="/">
+            Forgot password?
+          </Link>
+        </Stack>
+
+        <LoadingButton
+          fullWidth
+          size="large"
+          type="submit"
+          variant="contained"
+          loading={isSubmitting}
+        >
+          Login
+        </LoadingButton>
+      </FormProvider>
+    </Container>
+  );
+}
+
+export default LoginPage;
---
+++ src/routes/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import HomePage from "../pages/HomePage";
+import AccountPage from "../pages/AccountPage";
+import RegisterPage from "../pages/RegisterPage";
+import BlankLayout from "../layouts/BlankLayout";
+import NotFoundPage from "../pages/NotFoundPage";
+import LoginPage from "../pages/LoginPage";
+import AuthRequire from "./AuthRequire";
+
+function Router() {
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <AuthRequire>
+            <MainLayout />
+          </AuthRequire>
+        }
+      >
+        <Route index element={<HomePage />} />
+        <Route path="account" element={<AccountPage />} />
+        <Route path="user/:userId" element={<RegisterPage />} />
+      </Route>
+
+      <Route element={<BlankLayout />}>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default Router;
